1be40176a4e0fb10eff34c8f1b1f2b0d363a624b
Akeel Server <agilowen@gmail.com>
2011-08-07 15:53:50 -0600

    django-registration app installed and framework hooked up

--- urls.py
+++ urls.py
@@ -16,6 +16,7 @@ urlpatterns = patterns('',
 		(r'^videos/',include('videos.urls')),
 		(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 		(r'^faq/$','cs.views.faq'),
+		(r'^accounts/', include('registration.urls')),
 	# Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 	
